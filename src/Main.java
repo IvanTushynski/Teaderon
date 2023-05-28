db7d9b73165d5cf66166e5c4ac49ee3695ee17de
IvanTushynski <tushinski.ivan@gmail.com>
2023-05-28 15:35:10 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import data.ExpNeeded;
 import data.Experience;
 import data.MageAttributes;
 
@@ -9,12 +8,11 @@ public class Main {
         //Подгрузка классов-----------------------
         MageAttributes mageAttributes = new MageAttributes();
         Experience experience = new Experience();
-        ExpNeeded expAmount = new ExpNeeded();
         //----------------------------------------
 
         experience.saveLevelMain();
         System.out.println("Your Level is " + experience.loadLevelMain());
-        expAmount.setNeededExpMain();
+        experience.saveNeededExpMain();
         experience.loadLevelMain();
 
     }
--- src/data/Experience.java
+++ src/data/Experience.java
@@ -1,9 +1,7 @@
 package data;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Experience {
@@ -38,6 +36,31 @@ public class Experience {
         levelMain = level;
         scannerLoad.close();
         return level;
+    }
+
+    public void saveNeededExpMain() throws IOException {
+
+        ArrayList<Integer> numbers = null;
+        try (Scanner scanner = new Scanner(new File("src/saves/levelsMain"))) {
+            numbers = new ArrayList<>();
+            while (scanner.hasNextInt()) {
+                numbers.add(scanner.nextInt());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Integer[] array = numbers.toArray(new Integer[0]);
+
+        for (int i = 1; i < array[loadLevelMain()]; i++) {
+            System.out.println(array[loadLevelMain()]);
+            File file = new File("src/saves/expNeed");
+            PrintWriter printWriter = new PrintWriter(file);
+
+            printWriter.print(array[loadLevelMain()]);
+            printWriter.close();
+            break;
+        }
+
     }
 
 }
